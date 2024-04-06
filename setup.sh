b0210f581731735c934f028eb866ffc3c35fac2f
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-05 22:37:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-user=$(ls /home)
+userhome=/home/$(ls /home)/
+
+# Confirm install
+echo
+echo "This setup will remove"
+echo "- openbox"
+echo
+echo "This setup will install"
+echo "- nala"
+echo "- xorg"
+echo "- xterm"
+echo "- Ultimaker Cura"
+echo "======================="
+echo -n "Would you like to proceed? [Y/n] "
+read confirmed_install
+if [ "$confirmed_install" != "Y" ] && [ "$confirmed_install" != "y" ]; then
+	echo "Abort..."
+ 	exit 0
+fi
 
 # Upgrade system and install nala
 apt update && apt upgrade
@@ -11,13 +29,11 @@ nala remove openbox
 
 # Install necessary packages
 nala install xorg xterm
-nala install snapd
-snap install core
-snap install cura-slicer
+wget -P $userhome https://github.com/Ultimaker/Cura/releases/download/5.7.0/UltiMaker-Cura-5.7.0-linux-X64.AppImage
 
-nala install snapd
-snap install core
-snap install cura-slicer
-export XAUTHORITY=/home/$user/.Xauthority
-
-reboot
+# Confirm reboot
+echo -n "Would you like to reboot the system? [Y/n] "
+read confirmed_reboot
+if [ "$confirmed_reboot" == "Y" ] || [ "$confirmed_reboot" == "y" ]; then
+	reboot
+fi
